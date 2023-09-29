97131debc6458acfc630c79ac96f27f9cf9cf577
Arthur Lyra Miranda <82905393+blackjake13@users.noreply.github.com>
2023-09-28 23:08:54 -0300

    Add files via upload

---
+++ src/daodb4o/DAOApresentacao.java
@@ -0,0 +1,54 @@
+/**********************************
+ * IFPB - SI
+ * POB - Persistencia de Objetos
+ * Prof. Fausto Ayres
+ **********************************/
+package daodb4o;
+
+import java.util.List;
+
+import com.db4o.query.Query;
+
+import modelo.Apresentacao;
+
+public class DAOApresentacao  extends DAO<Apresentacao>{
+
+	public Apresentacao read (Object chave){
+		int id = (int) chave;	
+		Query q = manager.query();
+		q.constrain(Apresentacao.class);
+		q.descend("id").constrain(id);
+		List<Apresentacao> resultados = q.execute();
+		if (resultados.size()>0)
+			return resultados.get(0);
+		else
+			return null;
+	}
+
+	//metodo da classe DAO sobrescrito DAOAluguel para
+	//criar "id" sequencial 
+	public void create(Apresentacao obj){
+		int novoid = super.gerarId();  	//gerar novo id da classe
+		obj.setId(novoid);				//atualizar id do objeto antes de grava-lo no banco
+		manager.store( obj );
+	}
+
+	//--------------------------------------------
+	//  consultas de Aluguel
+	//--------------------------------------------
+
+	public List<Apresentacao> alugueisModelo(String modelo){
+		Query q;
+		q = manager.query();
+		q.constrain(Apresentacao.class);
+		q.descend("carro").descend("modelo").constrain(modelo);
+		return q.execute();
+	}
+
+	public List<Apresentacao> alugueisFinalizados(){
+		Query q = manager.query();
+		q.constrain(Apresentacao.class);
+		q.descend("finalizado").constrain(true);
+		return q.execute();
+	}
+}
---
+++ src/daodb4o/DAOCidade.java
@@ -0,0 +1,34 @@
+/**********************************
+ * IFPB - SI
+ * POB - Persistencia de Objetos
+ * Prof. Fausto Ayres
+ **********************************/
+
+package daodb4o;
+
+import java.util.List;
+
+import com.db4o.query.Query;
+
+import modelo.Cidade;
+
+public class DAOCidade extends DAO<Cidade>{
+
+	public Cidade read (Object chave){
+		String cpf = (String) chave;	//casting para o tipo da chave
+		Query q = manager.query();
+		q.constrain(Cidade.class);
+		q.descend("nome").constrain(nome);
+		List<Cidade> resultados = q.execute();
+		if (resultados.size()>0)
+			return resultados.get(0);
+		else
+			return null;
+	}
+
+	//--------------------------------------------
+	//  consultas
+	//--------------------------------------------
+	
+}
+
